d0aba8fedcfb4ce94b16599c59bc20065efd2318
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-07-05 10:27:31 +0500

    10:27 | 07/05/2024 | chore: Update file paths and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   target: "node",
-  entry: "./server.js",
+  entry: "./src/server.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
